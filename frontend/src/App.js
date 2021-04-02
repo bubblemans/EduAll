2bc33a102b59e3eec4a407d5493e79a2099667e7
Aryan Vaid <aryan.vaid@sjsu.edu>
2021-04-01 20:58:16 -0700

    Sidebar Refactored

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,11 @@
 import React, { useState } from 'react';
-import LoginForm from './Components/Loginform'
-import RegisterPage from './Components/RegisterPage';
-import HomePage from './Components/HomePage';
+import LoginForm from './Pages/Loginform'
+import RegisterPage from './Pages/RegisterPage';
+import Dashboard from './Pages/Dashboard';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import SideNav, { NavItem, NavIcon, NavText } from '@trendmicro/react-sidenav';
 import '@trendmicro/react-sidenav/dist/react-sidenav.css';
 import { ContextStore } from './ContextStore';
-import { withRouter } from 'react-router-dom'
+import Sidebar from './Components/Sidebar/Sidebar'
 
 export default function App() {
   const [ user , setUser ] = useState("");
@@ -14,48 +13,14 @@ export default function App() {
   return (
     <div className="App">
       <Router>
-      <Route render={({ location, history }) => (
-        <React.Fragment>
-          {showSidebar ? 
-          <SideNav
-              onSelect={(selected) => {
-                  const to = '/' + selected;
-                  if (location.pathname !== to) {
-                      history.push(to);
-                  }
-              }} 
-          >
-          <SideNav.Toggle />
-          <SideNav.Nav defaultSelected="home">
-              <NavItem eventKey="home">
-                  <NavIcon>
-                      <i className="fa fa-fw fa-home" style={{ fontSize: '1.75em' }} />
-                  </NavIcon>
-                  <NavText>
-                      Home Page 
-                  </NavText>
-              </NavItem>
-              <NavItem eventKey="devices">
-                  <NavIcon>
-                      <i className="fa fa-fw fa-device" style={{ fontSize: '1.75em' }} />
-                  </NavIcon>
-                  <NavText>
-                      Other Page
-                  </NavText>
-              </NavItem>
-            </SideNav.Nav>
-        </SideNav>
-        :
-        null
-        }
-        <ContextStore.Provider value={{ CurrentUser: [user,setUser], SideBar: [showSidebar, setShowSidebar]}}>
-            <Route path="/" exact component={withRouter(LoginForm)}/>
-            <Route path="/register" component={RegisterPage}/>
-            <Route path="/home" component={withRouter(HomePage)}/>
-        </ContextStore.Provider>
-      </React.Fragment>
-      )}
-      />
+        <Sidebar />
+        <Switch>        
+            <ContextStore.Provider value={{ CurrentUser: [user,setUser], SideBar: [showSidebar, setShowSidebar]}}>
+                <Route path="/" exact component={LoginForm}/>
+                <Route path="/register" component={RegisterPage}/>
+                <Route path="/dashboard" component={Dashboard}/>
+            </ContextStore.Provider>
+        </Switch>
       </Router>
     </div>
   );
