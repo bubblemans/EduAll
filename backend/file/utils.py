70dfa7c978322ba14cb54bda5a29b941eba353b1
Hung Yeh Lin <f89647779@gmail.com>
2021-04-04 23:16:05 +0800

    request user service

--- backend/file/utils.py
+++ backend/file/utils.py
@@ -8,5 +8,6 @@ API_URL = os.environ.get('API_URL')
 def get_user_id(token):
     # url = '{}/{}'.format(API_URL, token)
     # res = requests.get(API_URL)
-    # return res
+    # user_id = res['user_id']
+    # return user_id
     return token  # test
