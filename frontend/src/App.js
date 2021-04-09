5a21a68c54c7e5677acf4b69eadd0ea0eae051b7
Aryan Vaid <aryan.vaid@sjsu.edu>
2021-04-08 23:17:39 -0700

    Initial Setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import LoginForm from './Pages/Loginform'
+import SignupForm from './Pages/SignupForm'
 import RegisterPage from './Pages/RegisterPage';
 import Dashboard from './Pages/Dashboard';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
@@ -19,6 +20,7 @@ export default function App() {
         <Switch>        
             <ContextStore.Provider value={{ CurrentUser: [user,setUser], SideBar: [showSidebar, setShowSidebar]}}>
                 <Route path="/" exact component={LoginForm}/>
+                <Route path="/signup" exact component={SignupForm}/>
                 <Route path="/register" component={RegisterPage}/>
                 <Route path="/dashboard" component={Dashboard}/>
                 <Route path="/file" component={FileUpload}/>
