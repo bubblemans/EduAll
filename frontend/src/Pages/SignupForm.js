31ae618d73580d42bfcb2ed0f55645e351186626
MonicaNS <monica.saghar+github@gmail.com>
2021-04-09 01:22:51 -0700

    Sign up page completed

--- frontend/src/Pages/SignupForm.js
+++ frontend/src/Pages/SignupForm.js
@@ -3,10 +3,11 @@ import { ContextStore } from '../ContextStore';
 import { useHistory } from "react-router-dom";
 
 export default function SignupForm() {
-    const [formDetails, setFormDetails] = useState({email: "",password : ""})
+    const [formDetails, setFormDetails] = useState({email: "",password : "", firstName : "", lastName: ""})
     const { CurrentUser, SideBar } = useContext(ContextStore);
     const [ user, setUser ] = CurrentUser
     const [ showSidebar, setShowSidebar] = SideBar
+
     const history = useHistory()
     useEffect(() => {
         setShowSidebar(false)
@@ -21,14 +22,14 @@ export default function SignupForm() {
         let email = "admin@admin.com"
         let password = "admin123"
 
-        if(formDetails.email == email && formDetails.password == password){
-              console.log("Logged in");
+        if(formDetails.email != email && formDetails.password != password){
+              console.log("Registered. Now proceed to log in");
               setUser(formDetails.name)
               // Push to the Home Page 
-              history.push('/dashboard')
+              history.push('/')
 
         }else {
-            console.log("Details do not match!");
+            console.log("Sorry, user already exists.");
         }
     }
     return (
@@ -37,6 +38,14 @@ export default function SignupForm() {
                 <h2>EduALL</h2>
                 {/* {(error != "") ? (<div className = "error">{error}</div>) : ""} */}
                 <div className = "form-group">
+                    <label htmlFor ="firstName">First name: </label>
+                    <input type = "firstName" name = "firstName" id = "firstName"  onChange = {e => setFormDetails({...formDetails,firstName: e.target.value})} value={formDetails.firstName}/>
+                </div>
+                <div className = "form-group">
+                    <label htmlFor ="lastName">Last name: </label>
+                    <input type = "lastName" name = "lastName" id = "lastName"  onChange = {e => setFormDetails({...formDetails,lastName: e.target.value})} value={formDetails.lastName}/>
+                </div>
+                <div className = "form-group">
                     <label htmlFor ="email">Email :</label>
                     <input type = "email" name = "email" id = "email"  onChange = {e => setFormDetails({...formDetails,email: e.target.value})} value={formDetails.email}/>
                 </div>
@@ -44,7 +53,7 @@ export default function SignupForm() {
                     <label htmlFor ="password">Password :</label>
                     <input type = "password" name = "password" id = "password" onChange = {e => setFormDetails({...formDetails,password: e.target.value})} value={formDetails.password}/>
                 </div>
-                <input type = "submit" value = "Login"/>
+                <input type = "submit" value = "Register"/>
             </div>
         </form>
     )
