a94dcbc055aa38f666f7a7cefb29eeb13d6388be
Hung Yeh Lin <f89647779@gmail.com>
2021-04-16 22:14:13 +0800

    add dev environment automated set up

---
+++ install.sh
@@ -0,0 +1,17 @@
+# frontend
+cd ./frontend
+npm install
+
+# chat
+cd ../backend/chat
+npm install
+
+# chat-api
+cd ../chat-api
+npm install
+
+# course and user(springCrudAPI)
+
+# file
+cd ../file
+pip3 install -r requirements.txt
