2eef986ed73672f33430ea718055c28a3073f9cd
Aryan Vaid <aryan.vaid@sjsu.edu>
2021-04-26 17:31:19 -0700

    Dashboard UI redesign

--- frontend/src/Components/ClassCard.js
+++ frontend/src/Components/ClassCard.js
@@ -1,20 +1,22 @@
 import React from 'react'
-
-export default function ClassCard({ code, name, days, time, grade }) {
+import '../App.css'
+export default function ClassCard({ code, name, days, time, grade, color }) {
     return(
-        <div className="class-card">
-            <div className="class-code">{code}</div>
-            <div className="label">Class name</div>
-            <div className="class-details">{name}</div>
-            <div className="class-details-container">
-                <div style={{color:"grey"}}>Days</div> 
-                <div style={{color:"grey"}}>Time</div>
-            </div>
-            <div className="class-details-container">
-                <div>{days}</div> 
-                <div>{time}</div>
-            </div>
-            <div className="class-grade">Grade: {grade}</div>
+        <div className="class-card" style={{backgroundColor:color}}>
+        <div className="class-code" >{code}</div>
+        <div className="label">Class name</div>
+        <div className="class-details">{name}</div>
+        <div className="class-details-container">
+            <div style={{color:"white"}}>Days</div> 
+            <div style={{color:"white"}}>Time</div>
         </div>
-    ) 
+        <div className="class-details-container">
+            <div>{days}</div> 
+            <div>{time}</div>
+        </div>
+        <div className="label">Room name</div>
+        <div className="class-details">{code}</div>
+        <div className="class-grade">Grade: {grade}</div>
+    </div>
+    )
 }
