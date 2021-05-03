b2e3686a2f3127441e1ff6f7f02171eeb39d8b6e
Aryan Vaid <aryan.vaid@sjsu.edu>
2021-05-02 18:05:51 -0700

    Adding the useAlert Hook

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,11 +4,42 @@ import { BrowserRouter } from 'react-router-dom'
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { transitions, positions, Provider as AlertProvider } from 'react-alert'
+import AlertTemplate from 'react-alert-template-oldschool-dark'
+
+// const options = {
+//   // you can also just use 'bottom center'
+//   position: positions.BOTTOM_CENTER,
+//   timeout: 5000,
+//   offset: '30px',
+//   // you can also just use 'scale'
+//   transition: transitions.SCALE
+// }
+const alertOptions = {
+  offset: '25px',
+  timeout: 3000,
+  transition: transitions.SCALE
+}
+// const AlertTemplate = ({ style, options, message, close }) => (
+//   <div style={style}>
+//     {options.type === 'info' && '!'}
+//     {options.type === 'success' && ':)'}
+//     {options.type === 'error' && ':('}
+//     {message}
+//     <button onClick={close}>X</button>
+//   </div>
+// )
 
 ReactDOM.render(
+  <AlertProvider 
+      template={AlertTemplate}
+      position={positions.TOP_CENTER}
+      {...alertOptions}
+      >
   <BrowserRouter>
     <App />
-  </BrowserRouter>,
+  </BrowserRouter>
+  </AlertProvider>,
   document.getElementById('root')
 );
 
