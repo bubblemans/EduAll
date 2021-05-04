93b5596e531db2a06ff903b817d7a08b2efd9638
Hung Yeh Lin <f89647779@gmail.com>
2021-05-04 16:36:05 +0800

    separate automation scripts

--- deploy.sh
+++ deploy.sh
@@ -7,28 +7,5 @@ sudo systemctl start mongod &
 # redis
 sudo redis-server &
 
-# frontend
-cd ./frontend
-REACT_APP_BASE_URL=http://localhost npm start &
-
-# chat
-cd ../backend/chat
-npm start &
-
-# chat-api
-cd ../chat-api
-npm start &
-
-# course
-cd ../course
-mvn spring-boot:run &
-
-# user(springCrudAPI)
-cd ../springCrudAPI
-mvn spring-boot:run &
-
-# file
-cd ../file
-export FLASK_APP=app.py
-export MONGO_URI=mongodb://localhost:27017/eduAll
-flask run --host=0.0.0.0 &
+# all services
+sh update.sh
